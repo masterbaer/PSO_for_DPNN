e54128478edf3e8988ac7a358aec3b5599779c37
Muhammed <camandarli2@hotmail.de>
2023-04-30 13:18:21 +0200

    creating png from plot

--- code/sequential_learning/gradient_descent/result_plotter.py
+++ code/sequential_learning/gradient_descent/result_plotter.py
@@ -5,14 +5,14 @@ if __name__ == '__main__':
     loss = torch.load('seq_gd_loss.pt')
     plt.plot(loss)
     plt.ylabel("loss gd")
-    plt.show()
+    plt.savefig('seq_gd_loss.png')
 
     train_acc = torch.load('seq_gd_train_acc.pt')
     plt.plot(train_acc)
     plt.ylabel("training accuracy gd")
-    plt.show()
+    plt.savefig('seq_gd_train_acc.png')
 
     valid_acc = torch.load('seq_gd_valid_acc.pt')
     plt.plot(valid_acc)
     plt.ylabel("validation accuracy gd")
-    plt.show()
+    plt.savefig('seq_gd_valid_acc.png')
