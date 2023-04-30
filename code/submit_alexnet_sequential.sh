13f3b17b5c9ebf09f486d313631d700951352f9c
Muhammed <camandarli2@hotmail.de>
2023-04-30 16:06:12 +0200

    required time reduced

--- code/submit_alexnet_sequential.sh
+++ code/submit_alexnet_sequential.sh
@@ -3,7 +3,7 @@
 #SBATCH --job-name=alex_gpu_sequential
 #SBATCH --partition=gpu_4
 #SBATCH --gres=gpu:1   # number of requested GPUs (GPU nodes shared btwn multiple jobs)
-#SBATCH --time=2:00:00 # wall-clock time limit, adapt if necessary
+#SBATCH --time=45:00 # wall-clock time limit, adapt if necessary
 #SBATCH --mem=128000
 #SBATCH --nodes=1
 #SBATCH --mail-type=ALL
