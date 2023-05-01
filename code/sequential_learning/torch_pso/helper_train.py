4e87a1d74dba1385138b27df1f2edfc572faedc9
Muhammed Öz <camandarli2@hotmail.de>
2023-05-01 20:41:11 +0200

    torch-pso added

---
+++ code/sequential_learning/torch_pso/helper_train.py
@@ -0,0 +1,85 @@
+import torch
+import time
+
+from helper_evaluation import compute_accuracy
+
+
+# TRAIN MODEL (NON-PARALLEL)
+def train_model(model, num_epochs, train_loader,
+                valid_loader, test_loader, optimizer, criterion,
+                device, logging_interval=50,
+                scheduler=None):
+    start = time.perf_counter()
+
+    # Initialize history lists for loss, training accuracy, and validation accuracy.
+    loss_history, train_acc_history, valid_acc_history = [], [], []
+
+    # ACTUAL TRAINING STARTS HERE.
+    for epoch in range(num_epochs):  # Loop over epochs.
+
+        model.train()  # Set model to training mode.
+
+        #  Thus, layers like dropout, batchnorm etc. which behave differently on
+        #  train and test procedures know what is going on and can behave accordingly.
+
+        for batch_idx, (features, targets) in enumerate(train_loader):  # Loop over mini batches.
+
+            # CONVERT DATASET TO USED DEVICE.
+            features = features.to(device)
+            targets = targets.to(device)
+
+            # the closure method is needed when the function has to be reevaluated multiple times
+            # see https://pytorch.org/docs/stable/optim.html
+            # and https://github.com/qthequartermasterman/torch_pso for the usage
+
+            def closure():
+                optimizer.zero_grad()
+                predictions = model(features)
+                loss = criterion(predictions, targets)
+                # loss.backward()
+                return loss
+
+            predictions = model(features)
+            loss = criterion(predictions, targets)
+            optimizer.step(closure())
+
+            loss_history.append(loss.item())  # Logging.
+
+            if not batch_idx % logging_interval:
+                print(f'Epoch: {epoch + 1:03d}/{num_epochs:03d} '
+                      f'| Batch {batch_idx:04d}/{len(train_loader):04d} '
+                      f'| Loss: {loss:.4f}')
+
+        # VALIDATION STARTS HERE.
+        #
+        # Set model to evaluation mode.
+        model.eval()
+
+        with torch.no_grad():  # Context-manager that disables gradient calculation to reduce memory consumption.
+
+            # COMPUTE ACCURACY OF CURRENT MODEL PREDICTIONS ON TRAINING + VALIDATION DATASETS.
+            train_acc = compute_accuracy(model, train_loader, device=device)  # Compute accuracy on training data.
+            valid_acc = compute_accuracy(model, valid_loader, device=device)  # Compute accuracy on validation data.
+
+            print(f'Epoch: {epoch + 1:03d}/{num_epochs:03d} '
+                  f'| Train: {train_acc :.2f} '
+                  f'| Validation: {valid_acc :.2f}')
+
+            train_acc_history.append(train_acc.item())  # Append training accuracy to history list
+            valid_acc_history.append(valid_acc.item())  # Append validation accuracy to history list.
+
+        elapsed = (time.perf_counter() - start) / 60  # Measure training time per epoch.
+        print(f'Time elapsed: {elapsed:.2f} min')
+
+        if scheduler is not None:
+            scheduler.step(valid_acc_history[-1])
+
+    elapsed = (time.perf_counter() - start) / 60
+    print(f'Total Training Time: {elapsed:.2f} min')
+
+    # FINAL TESTING STARTS HERE.
+    #
+    test_acc = compute_accuracy(model, test_loader, device=device)  # Compute accuracy on test data.
+    print(f"Test accuracy {test_acc :.2f}")
+
+    return loss_history, train_acc_history, valid_acc_history
---
+++ code/sequential_learning/torch_pso/main.py
@@ -0,0 +1,94 @@
+###########
+# IMPORTS #
+###########
+
+import torch
+import torchvision
+from torch_pso import ParticleSwarmOptimizer
+
+from dataloader import get_dataloaders_cifar10, set_all_seeds
+from helper_train import train_model
+import result_plotter
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+
+    ############
+    # SETTINGS #
+    ############
+
+    seed = 123  # Set random seed.
+    b = 256  # Set batch size.
+    e = 40  # Set number of epochs to be trained. Standard: 200 epochs
+
+    # Get device used for training, e.g., check via torch.cuda.is_available().
+    device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')  # Set device.
+    print(f'Using {device} device.')
+    set_all_seeds(seed)  # Set all seeds to chosen random seed.
+
+    ###########
+    # DATASET #
+    ###########
+
+    # Using transforms on your data allows you to transform it from its
+    # source state so that it is ready for training/validation/testing.
+
+    train_transforms = torchvision.transforms.Compose([
+        torchvision.transforms.Resize((70, 70)),
+        torchvision.transforms.RandomCrop((64, 64)),
+        torchvision.transforms.ToTensor(),
+        torchvision.transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+    ])
+
+    test_transforms = torchvision.transforms.Compose([
+        torchvision.transforms.Resize((70, 70)),
+        torchvision.transforms.CenterCrop((64, 64)),
+        torchvision.transforms.ToTensor(),
+        torchvision.transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+    ])
+
+    # GET PYTORCH DATALOADERS FOR TRAINING, TESTING, AND VALIDATION DATASET.
+    train_loader, valid_loader, test_loader = get_dataloaders_cifar10(
+        batch_size=b,
+        root="../../data",
+        validation_fraction=0.1,
+        train_transforms=train_transforms,
+        test_transforms=test_transforms,
+        num_workers=0
+    )
+
+    # Check loaded data.
+    for images, labels in train_loader:
+        print('Image batch dimensions:', images.shape)
+        print('Image label dimensions:', labels.shape)
+        print('Class labels of 10 examples:', labels[:10])
+        break
+
+    ########
+    # MAIN #
+    ########
+
+    model = torchvision.models.AlexNet(num_classes=10)
+    model = model.to(device)
+
+    optimizer = ParticleSwarmOptimizer(model.parameters(),
+                                       inertial_weight=0.5,
+                                       num_particles=100,
+                                       max_param_value=1,
+                                       min_param_value=-1)
+    criterion = torch.nn.CrossEntropyLoss()
+
+    # TRAIN MODEL.
+    loss_list, train_acc_list, valid_acc_list = train_model(model=model, num_epochs=e,
+                                                            train_loader=train_loader, valid_loader=valid_loader,
+                                                            test_loader=test_loader,
+                                                            optimizer=optimizer, criterion=criterion, device=device,
+                                                            logging_interval=100)
+
+    # Save history lists for loss, training accuracy, and validation accuracy to files.
+    torch.save(loss_list, 'torch_pso_loss.pt')
+    torch.save(train_acc_list, 'torch_pso_train_acc.pt')
+    torch.save(valid_acc_list, 'torch_pso_valid_acc.pt')
+    result_plotter.save_loss(loss_list)
+    result_plotter.save_train_acc(train_acc_list)
+    result_plotter.save_valid_acc(valid_acc_list)
