680ce629a4812553ce6a3d6fa1dc2c8cf02c89bc
Muhammed <camandarli2@hotmail.de>
2023-07-16 22:51:04 +0200

    plotter

--- code/dataparallel_learning/particle_swarm_opt/experiment19_plotter.py
+++ code/dataparallel_learning/particle_swarm_opt/experiment19_plotter.py
@@ -3,26 +3,12 @@ from matplotlib import pyplot as plt
 import numpy as np
 
 if __name__ == '__main__':
+
     accuracies_true = torch.load("experiment19_accuracy_True.pt")
-    X = np.arange(0, 5000, 5)
-    plt.plot(X, accuracies_true)
-    plt.savefig("experiment19_accuracy_True.png")
-    plt.cla()
-
-    losses_true = torch.load("experiment19_loss_True.pt")
-    X = np.arange(0, 5000, 5)
-    plt.plot(X, losses_true)
-    plt.savefig("experiment19_loss_True.png")
-    plt.cla()
-
     accuracies_false = torch.load("experiment19_accuracy_False.pt")
     X = np.arange(0, 5000, 5)
-    plt.plot(X, accuracies_false)
-    plt.savefig("experiment19_accuracy_False.png")
-    plt.cla()
-
-    losses_false = torch.load("experiment19_loss_False.pt")
-    X = np.arange(0, 5000, 5)
-    plt.plot(X, losses_false)
-    plt.savefig("experiment19_loss_False.png")
+    plt.plot(X, accuracies_true, color="r", label="using momentum on pull")
+    plt.plot(X, accuracies_false, color="g", label="momentum on only gradients")
+    plt.legend()
+    plt.savefig("experiment19_accuracies.png")
     plt.cla()
