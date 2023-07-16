c62705319b4a3fb4b2c1bcd0cd8fef9ef391844b
Muhammed <camandarli2@hotmail.de>
2023-07-16 21:34:53 +0200

    plotter

--- code/dataparallel_learning/particle_swarm_opt/experiment19_plotter.py
+++ code/dataparallel_learning/particle_swarm_opt/experiment19_plotter.py
@@ -3,12 +3,26 @@ from matplotlib import pyplot as plt
 import numpy as np
 
 if __name__ == '__main__':
-    accuracies = torch.load("experiment19_accuracy_True.pt")
+    accuracies_true = torch.load("experiment19_accuracy_True.pt")
     X = np.arange(0, 5000, 5)
-    plt.plot(X, accuracies)
+    plt.plot(X, accuracies_true)
     plt.savefig("experiment19_accuracy_True.png")
+    plt.cla()
 
-    losses = torch.load("experiment19_loss_True.pt")
+    losses_true = torch.load("experiment19_loss_True.pt")
     X = np.arange(0, 5000, 5)
-    plt.plot(X, losses)
+    plt.plot(X, losses_true)
     plt.savefig("experiment19_loss_True.png")
+    plt.cla()
+
+    accuracies_false = torch.load("experiment19_accuracy_False.pt")
+    X = np.arange(0, 5000, 5)
+    plt.plot(X, accuracies_false)
+    plt.savefig("experiment19_accuracy_False.png")
+    plt.cla()
+
+    losses_false = torch.load("experiment19_loss_False.pt")
+    X = np.arange(0, 5000, 5)
+    plt.plot(X, losses_false)
+    plt.savefig("experiment19_loss_False.png")
+    plt.cla()
