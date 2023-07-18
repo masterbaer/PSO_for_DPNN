35b40be7d74e9f99fb40d44f06b2b66976fd0943
Muhammed <camandarli2@hotmail.de>
2023-07-18 10:34:17 +0200

    trying a social factor of 0.5

--- code/submit_experiment16.sh
+++ code/submit_experiment16.sh
@@ -3,7 +3,7 @@
 #SBATCH --job-name=experiment16         # job name
 #SBATCH --partition=multiple            # queue for resource allocation
 #SBATCH --nodes=4                       # number of nodes to be used
-#SBATCH --time=00:08:00                   # wall-clock time limit, set to 2h
+#SBATCH --time=00:03:00                   # wall-clock time limit, set to 2h
 #SBATCH --mem=4000                     # memory per node
 #SBATCH --cpus-per-task=1              # number of CPUs required per MPI task
 #SBATCH --ntasks-per-node=1             # maximum count of tasks per node
@@ -18,8 +18,9 @@ module load devel/cuda/10.2
 
 source venv/bin/activate      # Activate your virtual environment.
 
+mpirun python -u dataparallel_learning/particle_swarm_opt/experiment16_random_init.py 0.5
 mpirun python -u dataparallel_learning/particle_swarm_opt/experiment16_random_init.py 0.0 # social weight is 0 --> independent local sgds
 mpirun python -u dataparallel_learning/particle_swarm_opt/experiment16_random_init.py 0.01
 mpirun python -u dataparallel_learning/particle_swarm_opt/experiment16_random_init.py 0.1
-mpirun python -u dataparallel_learning/particle_swarm_opt/experiment16_random_init.py 0.5
+
 mpirun python -u dataparallel_learning/particle_swarm_opt/experiment16_random_init.py 1.0 # every particle is assigned to the average
