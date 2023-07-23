0d237fe1ab759c357c455ae19e5430a3135b5422
Muhammed <camandarli2@hotmail.de>
2023-07-23 13:37:58 +0200

    trying different init

--- code/dataparallel_learning/particle_swarm_opt/experiment20_plotter.py
+++ code/dataparallel_learning/particle_swarm_opt/experiment20_plotter.py
@@ -5,11 +5,13 @@ import numpy as np
 if __name__ == '__main__':
 
     accuracies = torch.load("experiment20_accuracy.pt")
+    accuracies_random_init = torch.load("experiment20_accuracy_random_init.pt")
     accuracies_sequential = torch.load("simple_gd_accuracy_0.1_256.pt")  # produced in sequential learning folder
 
     X = np.arange(0, 5000, 20)
     plt.plot(X, accuracies, label="pso_average_pull", color="r")
     plt.plot(X, accuracies_sequential, label="seq", color="g")
+    plt.plot(X, accuracies_random_init, label="pso_average_pull_random_init", color="b")
     plt.legend()
-    plt.savefig("experiment20_accuracies.png")
+    plt.savefig("experiment20_accuracies_random.png")
     plt.cla()
