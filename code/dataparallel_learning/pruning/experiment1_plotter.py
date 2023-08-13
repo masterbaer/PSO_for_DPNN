9b5e801f33e39c1924d77dd20b9b2a70b04e1369
Muhammed <camandarli2@hotmail.de>
2023-08-13 13:10:46 +0200

    plotter

--- code/dataparallel_learning/pruning/experiment1_plotter.py
+++ code/dataparallel_learning/pruning/experiment1_plotter.py
@@ -4,10 +4,17 @@ from matplotlib import pyplot as plt
 if __name__ == '__main__':
 
     accuracies_model0 = torch.load("ex1_test_accuracy0.pt")
+    accuracies_model1 = torch.load("ex1_test_accuracy1.pt")
+    accuracies_model2 = torch.load("ex1_test_accuracy2.pt")
+    accuracies_model3 = torch.load("ex1_test_accuracy3.pt")
+
     accuracies_combined = torch.load("ex1_combined_test_accuracy.pt")
     accuracies_combined_scratch = torch.load("ex1_combined_scratch_test_accuracy.pt")
 
     plt.plot(accuracies_model0, label="model 0")
+    plt.plot(accuracies_model1, label="model 1")
+    plt.plot(accuracies_model2, label="model 2")
+    plt.plot(accuracies_model3, label="model 3")
     plt.plot(accuracies_combined, label="combined model")
     plt.plot(accuracies_combined_scratch, label="big model from scratch")
 
